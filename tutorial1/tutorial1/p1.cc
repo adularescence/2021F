41b936c841931de917c471386a7981031f440b7e
Kevin Li <46378390+adularescence@users.noreply.github.com>
2021-09-16 18:14:30 +0000

    feat: p1 done

---
+++ tutorial1/tutorial1/p1.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	string username;
+	cout << "Hi! What is your name? ";
+	cin >> username;
+	cout << "Hello " << username << "!" << endl;
+	return 0;
+}
