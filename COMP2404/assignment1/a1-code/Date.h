d5b6c73a7f5ea2329ffb09a4e82e5a827a578370
Kevin Li <46378390+adularescence@users.noreply.github.com>
2021-09-28 15:50:54 -0400

    style: format Date.cc and Date.h
    A1

--- COMP2404/assignment1/a1-code/Date.h
+++ COMP2404/assignment1/a1-code/Date.h
@@ -5,47 +5,47 @@
 #include <iostream>
 #include <string>
 
+const int MAX_DURATION = 3;
+
 using namespace std;
 
 class Date {
-		
-	public:
-		//constructor
-		Date();
-		Date(int year, int month, int day);
-		
-				
-		//setters
-		void setDay( int);
-		void setMonth(int);
-		void setYear(int);
-		void setDate(int, int, int);
-		void setDate(Date&);
-		
-		//getters
-		int getDay();
-		int getMonth();
-		int getYear();
-		const string& getMonthName();
-		
-		//other
-		bool lessThan(Date& d);
-		void print();
-	
-	private:
-		//functions
-		int getMaxDay();
-	
-	
-		//variables
-		int day;
-		int month;
-		int year;
-		int hour;
-		int duration;
-		
-		const string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		const string smonths[12]={"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"};
-	
+    public:
+        //constructor
+        Date();
+        Date(int year, int month, int day);
+        
+                
+        //setters
+        void setDay( int);
+        void setMonth(int);
+        void setYear(int);
+        void setDate(int, int, int);
+        void setDate(Date&);
+
+        //getters
+        int getDay();
+        int getMonth();
+        int getYear();
+        const string& getMonthName();
+
+        //other
+        bool lessThan(Date& d);
+        void print();
+
+    private:
+        //functions
+        int getMaxDay();
+
+        //variables
+        int day;
+        int month;
+        int year;
+        int hour;
+        int duration;
+
+        const string months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        const string smonths[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"};
+
 };
 #endif
