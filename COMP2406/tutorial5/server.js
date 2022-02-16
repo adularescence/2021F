44fcefba097759517499d31c3ee1144eb35b761d
adularescence <46378390+adularescence@users.noreply.github.com>
2022-02-15 22:44:18 -0500

    feat: COMP2406 t5 done

--- COMP2406/tutorial5/server.js
+++ COMP2406/tutorial5/server.js
@@ -1,39 +1,122 @@
-/*
-Card source: https://api.hearthstonejson.com/v1/25770/enUS/cards.json
+const http = require("http"); // import the http module, so that we can create a web server
+const file = require("fs"); // import the fs (file system) module so that we read and write data to files
+const url = require("url"); // import the url module so we can parse the web address of the request into readable parts
 
-Each card is a JS object
-All have:
-	id - string uniquely identifies the card
-	artist - string indicating the name of the artist for the cards image
-	cardClass - string indicating the class of the card
-	set  - string indicating the set the card is from
-	type - string indicating the type of the card
-	text - string indicating card text
-Some have:
-	rarity - string indicating the rarity of the card
-	mechanics - array of string indicating special mechanics
-	
-Routes:
-	/cards - search all cards (query params: class, set, type, artist)
-	/cards/:cardID - specific card with ID=:cardID
-*/
 
-const http = require('http');
+const host = "localhost"; // address of the server; localhost means that the server is referring to itself and is not accessible from the internet
+const port = 8000; // port most commonly used by webservers
 
-//Set up the required data
-let cardData = require("./cards.json");
-let cards = {}; //Stores all of the cards, key=id
-cardData.forEach(card => {
-	cards[card.id] = card;
+// process a request received, prepare and send a response back to the client
+const processRequest = (request, response) => {
+    const urlObject = url.parse(request.url, true); // parses the URL into readable parts
+
+    // TODO 1: receive the request for the index.html web page, read the contents of the index.html webpage from the file system, and then send the contents of the web page as a response to the client
+    if (urlObject.pathname === "/index.html") {
+        file.readFile("index.html", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            response.writeHeader(200, {
+                "Content-Type": "text/html"
+            });
+            response.write(data);
+            response.end();
+        });
+    }
+
+    // TODO 2: receive the request for the t5.css CSS file, read contents of the t5.css file from the file system, and then send the contents of this file as a response to the client
+    else if (urlObject.pathname === "/t5.css") {
+        file.readFile("t5.css", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            response.writeHeader(200, {
+                "Content-Type": "text/css"
+            });
+            response.write(data);
+            response.end();
+        });
+    }
+
+    // TODO 3: receive the request for the t5.js JavaScript file, read contents of the t5.js file from the file system, and then send the contents of this file as a response to the client
+    else if (urlObject.pathname === "/t5.js") {
+        file.readFile("t5.js", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            response.writeHeader(200, {
+                "Content-Type": "text/javascript"
+            });
+            response.write(data);
+            response.end();
+        });
+    }
+
+    // TODO 4: receive the request for the students.js JavaScript file, read contents of the students.js file from the file system, and then send the contents of this file as a response to the client
+    // else if (urlObject.pathname === "/students.js") {
+    //     file.readFile("students.js", (err, data) => {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         response.writeHeader(200, {
+    //             "Content-Type": "text/javascript"
+    //         });
+    //         response.write(data);
+    //         response.end();
+    //     });
+    // }
+
+    // TODO 5: receive the request for the students.json JSON file, read contents of the file from the file system, and then send the contents of this file as a response to the client
+    else if (urlObject.pathname === "/students.json") {
+        file.readFile("students.json", (err, data) => {
+            if (err) {
+                throw err;
+            }
+            response.writeHeader(200, {
+                "Content-Type": "application/json"
+            });
+            response.write(data);
+            response.end();
+        });
+    }
+
+    else if (urlObject.pathname === "/newStudent" && request.method === "POST") {
+        let body = "";
+        request.on("data", (data) => {
+            body += data;
+        });
+        request.on("end", () => {
+            const newStudent = JSON.parse(body);
+            updateStudents(newStudent);
+
+            response.writeHeader(201);
+            response.end();
+        })
+    }
+}
+
+const updateStudents = (newStudent) => {
+    file.readFile("students.json", (readErr, data) => {
+        if (readErr) {
+            throw readErr;
+        }
+        const studentsJSON = JSON.parse(data.toString());
+        studentsJSON.students.push(newStudent);
+        
+        const buffer = Buffer.from(JSON.stringify(studentsJSON), "utf-8");
+        file.writeFile("students.json", buffer, (writeErr) => {
+            if (writeErr) {
+                throw writeErr;
+            }
+        });
+    });
+};
+
+// run the server: node server.js
+// if you make a change to your server code, you must restart the server
+
+const server = http.createServer(processRequest); // create the server object
+
+server.listen(port, host, () => { // Bind the port and host to the server
+    console.log("Server is running!");
 });
-
-//Initialize server
-const server = http.createServer(function (request, response) {
-	response.statusCode = 404;
-	response.write("Unknwn resource.");
-	response.end();
-});
-
-//Start server
-server.listen(3000);
-console.log("Server listening at http://localhost:3000");
---
+++ COMP2406/tutorial5/t5.js
@@ -0,0 +1,185 @@
+const checkTextField_ids = ["snum", "fname", "lname", "agrade", "tgrade", "egrade"];
+let students = [];
+
+// see line 168
+const afterLoaded = () => {
+    retrieveStudentData()
+    checkTextField_ids.forEach((elem) => {
+        document.getElementById(elem).addEventListener("input", checkTextField);
+    });
+};
+
+
+// adds/removes classes from element according to becomeValid
+const setValidity = (element, becomeValid) => {
+    if (becomeValid) {
+        element.classList.add("valid");
+        element.classList.remove("invalid");
+    } else {
+        element.classList.add("invalid");
+        element.classList.remove("valid");
+    }
+}
+
+const checkTextField = (event) => {
+    const target = event.target;
+    const value = target.value;
+    switch (target.id) {
+        case "snum":
+            // valid student IDs are 9 digits
+            if (value.match(/^\d{9}$/) === null) {
+                setValidity(target, false);
+            } else {
+                setValidity(target, true);
+            }
+            break;
+        case "fname":
+        case "lname":
+            // first/last names should start with a capital letter, then any number of lower case letters
+            if (value.match(/^[A-Z][a-z]+$/) === null) {
+                setValidity(target, false);
+            } else {
+                setValidity(target, true);
+            }
+            break;
+        case "agrade":
+        case "tgrade":
+        case "egrade":
+            // any grade can has up to 3 digits
+            // then . and any number of digits after (optional)
+            if (value.match(/^\d{1,3}(\.\d*)?$/) === null) {
+                setValidity(target, false);
+            } else {
+                // ensure that the grade is between 0 and 100, inclusive
+                const grade_float = parseFloat(value, 10);
+                if (grade_float < 0 || grade_float > 100) {
+                    setValidity(event.target, false);
+                } else {
+                    setValidity(event.target, true);
+                }
+            }
+            break;
+    }
+
+    // if all fields are valid, enable the submit button
+    // else dont
+    const allGreen = document.getElementsByClassName("valid").length === 6;
+    const btnSubmit = document.getElementById("btnSubmit");
+    if (allGreen) {
+        btnSubmit.disabled = false;
+    } else {
+        btnSubmit.disabled = true;
+    }
+};
+
+// populate the table with student data from students.js file. We're reading information
+// from an on object and using that to populate the table
+const populateStudents = () => {
+    const table = document.querySelector("table");
+    const trs = Object.values(document.getElementsByTagName("tr"));
+    for (let i = 1; i < trs.length; i++) {
+        trs[i].remove();
+    }
+
+    // iterate through all the objects in the students array
+    for (let student of students) {
+        // save the information for the current in variables
+        let studentID = student.snum;
+        let firstName = student.fname;
+        let lastName = student.lname;
+        let assignmentGrade = student.agrade;
+        let tutorialGrade = student.tgrade;
+        let examGrade = student.egrade;
+
+        // time to create a new HTML element!
+        // 1). we first need to create a new row
+        let row = document.createElement("tr");
+
+        // create a cell for the student ID, update its text value, and append it to the row
+        let iDCell = document.createElement("td");
+        iDCell.textContent = studentID;
+        row.appendChild(iDCell);
+
+        // create a cell for the first name, update its text value, and append it to the row
+        let fNameCell = document.createElement("td");
+        fNameCell.textContent = firstName;
+        row.appendChild(fNameCell);
+
+        // create a cell for the last name, update its text value, and append it to the row
+        let lNameCell = document.createElement("td");
+        lNameCell.textContent = lastName;
+        row.appendChild(lNameCell);
+
+        // create a cell for the assignment grade, update its text value, and append it to the row
+        let aGradeCell = document.createElement("td");
+        aGradeCell.textContent = assignmentGrade.toFixed(2);
+        row.appendChild(aGradeCell);
+
+
+        // create a cell for the tutorial grade, update its text value, and append it to the row
+        let tGradeCell = document.createElement("td");
+        tGradeCell.textContent = tutorialGrade.toFixed(2);
+        row.appendChild(tGradeCell);
+
+        // create a cell for the exam grade, update its text value, and append it to the row
+        let eGradeCell = document.createElement("td");
+        eGradeCell.textContent = examGrade.toFixed(2);
+        row.appendChild(eGradeCell);
+
+        // append the row to the table
+        table.appendChild(row);
+    }
+};
+
+const retrieveStudentData = () => {
+    const xhttp = new XMLHttpRequest;
+    xhttp.open("GET", "http://localhost:8000/students.json", true);
+    xhttp.send();
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200) {
+            const data = xhttp.responseText;
+            const studentsJSON = JSON.parse(data);
+            students = studentsJSON.students;
+            populateStudents();
+        }
+    };
+};
+
+const sendStudentData = () => {
+    const newStudent = {};
+    checkTextField_ids.forEach((id) => {
+        const content = document.getElementById(`${id}`).value;
+        switch (id) {
+            case "snum":
+            case "fname":
+            case "lname":
+                newStudent[`${id}`] = content;
+                break;
+            case "agrade":
+            case "tgrade":
+            case "egrade":
+                const truncated = parseFloat(content, 10);
+                newStudent[`${id}`] = truncated;
+                break;
+        };
+    });
+
+    const xhttp = new XMLHttpRequest;
+    xhttp.open("POST", "http://localhost:8000/newStudent", true);
+    xhttp.setRequestHeader("Content-Type", 'application/json');
+    xhttp.send(JSON.stringify(newStudent));
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 201) {
+            retrieveStudentData();
+        }
+    };
+};
+
+
+// apparently, the HTML usually isn't loaded by the time the JS is ready to run
+// so we call a function with all the setup stuff after it loads
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", afterLoaded);
+} else {
+    afterLoaded();
+}
